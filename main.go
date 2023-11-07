d23771279c1ab72e717e57253af392939183362d
adust09 <shoukitsuda@icloud.com>
2023-11-07 14:20:53 +0900

    generate Keygen func

--- main.go
+++ main.go
@@ -1,49 +1,46 @@
 package main
 
 import (
-	"math/big"
+	"fmt"
 	"time"
 
-	"github.com/bnb-chain/tss-lib/common"
 	"github.com/bnb-chain/tss-lib/ecdsa/keygen"
-	"github.com/bnb-chain/tss-lib/ecdsa/signing"
 	"github.com/bnb-chain/tss-lib/tss"
 )
 
-func Main() {
-	ids := []tss.UnSortedPartyIDs{}
-	parties := tss.SortPartyIDs(ids[0], 0)
-	partyIDMap := make(map[string]*tss.PartyID)
-	for _, id := range parties {
-		partyIDMap[id.Id] = id
-	}
+func main() {
+	start := time.Now()
 
-	curve := tss.S256()
-	ctx := tss.NewPeerContext(parties)
-	thisParty := tss.NewPartyID("Bitcoin", "BTC", big.NewInt(1))
-	params := tss.NewParameters(curve, ctx, thisParty, len(parties), 3)
+	pIDs := tss.GenerateTestPartyIDs(5)
+	p2pCtx := tss.NewPeerContext(pIDs)
+	threshold := 2
+	params := tss.NewParameters(tss.S256(), p2pCtx, pIDs[0], len(pIDs), threshold)
 
 	KeyGen(params)
-	Sign(params)
+
+	end := time.Now()
+	elapsed := end.Sub(start)
+	fmt.Printf("実行時間: %s\n", elapsed)
+
+	// Sign(params)
 }
 
 func KeyGen(params *tss.Parameters) {
 	outCh := make(chan tss.Message, 100)
 	endCh := make(chan keygen.LocalPartySaveData, 1)
 	preParams, _ := keygen.GeneratePreParams(1 * time.Minute)
-
 	party := keygen.NewLocalParty(params, outCh, endCh, *preParams)
 
 	party.Start()
 
 }
 
-func Sign(params *tss.Parameters) {
-	keys, _, _ := keygen.LoadKeygenTestFixturesRandomSet(3, 10)
+// func Sign(params *tss.Parameters) {
+// 	keys, signPIDs, _ := keygen.LoadKeygenTestFixturesRandomSet(testThreshold+1, testParticipants)
 
-	outCh := make(chan tss.Message, 100)
-	endCh := make(chan common.SignatureData, 1)
+// 	outCh := make(chan tss.Message, 100)
+// 	endCh := make(chan common.SignatureData, 1)
 
-	party := signing.NewLocalParty(big.NewInt(42), params, keys[0], outCh, endCh)
-	party.Start()
-}
+// 	party := signing.NewLocalParty(big.NewInt(42), params, keys[0], outCh, endCh)
+// 	party.Start()
+// }
